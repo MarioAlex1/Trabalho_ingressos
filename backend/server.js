1716b09fc8ea241401ad33e44a90cbe8cba038d4
MarioAlex1 <alexxbarbosa662@gmail.com>
2025-09-21 12:55:30 -0300

    Adicionando metodos para comunicar o back e o front

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors"); // pra comunicar com o frontend
 const app = express();
 const sequelize = require("./src/config");
 const usuarioRoutes = require("./src/routes");
 const { Usuario, Evento, Ingresso } = require("./src/models/associations");
+
+app.use(cors()); // permite comunicação com o frontend
 
 app.use(express.json());
 app.use("/api", usuarioRoutes);
@@ -14,3 +17,6 @@ sequelize.sync()
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
+
+//pra o front comunicar com o back vai precisar de axios
+//assim como o back precisa do cors pra permitir a comunicação com o front
